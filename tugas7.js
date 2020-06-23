08ef75031b9ee182cac4fb85e6ea6a0ba0d40477
maitasawilia <maitasaw@gmail.com>
2020-06-23 22:43:01 +0700

    Tugas 7 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+var angka= [140,145,150,155,160,165,170,175,180,185];
+for (let x of angka){
+  console.log(x)
+}
+
+var angka = [140,145,150,155,160,165,170,175,180,185];
+for (var i=0;i<angka.length;i++){
+  console.log(angka[i])
+}
